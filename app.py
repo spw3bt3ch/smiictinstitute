e8438f949bb524c1337e407ffe9fd92ebed31539
Samuel Ogunjimi <42714945+spw3bt3ch@users.noreply.github.com>
2025-05-04 15:51:21 +0000

    Site content newly added

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-  return render_template('home.html', template_folder='templates')
+def index():
+  return render_template("index.html")
 
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True)
+  app.run(debug=True, host="0.0.0.0", port=5000)
